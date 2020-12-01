8744ea258489a426e662c717e4adb5149ebd0804
Adriaan Putter <adriaan.putter@serko.com>
2020-12-02 09:42:46 +1300

    Day 1 first cut

---
+++ day-one/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace day_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = File.ReadAllLines("input.txt");
+            foreach (var number1 in input)
+            {
+                foreach (var number2 in input)
+                {
+                    foreach (var number3 in input)
+                    {
+                        var a = int.Parse(number1);
+                        var b = int.Parse(number2);
+                        var c = int.Parse(number3);
+                        if (a + b + c == 2020)
+                        {
+                            Console.WriteLine($"{number1} + {number2} + {number3} = 2020");
+                            Console.WriteLine(a * b * c);
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
