90e1d1f229c4cc27cd6b24698b385942558d78f7
Adriaan Putter <adriaan.putter@gmail.com>
2020-12-03 23:33:51 +1300

    Day 3 part 1

---
+++ day-three/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+var input = File.ReadAllLines("input.txt");
+
+var numberOfTrees = 0;
+var pointer = 3;
+var currentLine = 1;
+
+while (currentLine < input.Length)
+{
+    var line = input[currentLine];
+    Console.WriteLine($"{currentLine}-{pointer}: {line} --> {pointer} = {line[pointer]}");
+
+    if (line[pointer] == '#')
+    {
+        numberOfTrees++;
+    }
+
+    pointer += 3;
+    if (pointer > line.Length - 1)
+    {
+        pointer = pointer - line.Length;
+    }
+    currentLine++;
+}
+
+Console.WriteLine($"{numberOfTrees} Trees");
