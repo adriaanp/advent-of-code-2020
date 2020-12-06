2cdb8c093fbf58302cdefaa34ca845a5ef2f87e1
Adriaan Putter <adriaan.putter@gmail.com>
2020-12-06 20:29:36 +1300

    Day 4 part 2

--- day-four/Program.cs
+++ day-four/Program.cs
@@ -4,20 +4,40 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
 
-
 var passports = LoadPassports();
 
 Console.WriteLine($"Total passports: {passports.Count()}");
 Console.WriteLine($"Valid Passports: {passports.Count(IsPassportValid)}");
-
-
 
 bool IsPassportValid(Dictionary<string, string> passport)
 {
     var requiredFields = new List<string>(){
         "byr", "iyr","eyr","hgt","hcl","ecl", "pid"
     };
-    return requiredFields.All(passport.ContainsKey);
+    return requiredFields.All(passport.ContainsKey) && ValidateFields(passport);
+}
+
+bool ValidateFields(Dictionary<string, string> passport)
+{
+    var validators = new Dictionary<string, Func<string, bool>>()
+    {
+        {"byr", value => int.Parse(value) is >= 1920 and <= 2002},
+        {"iyr", value => int.Parse(value) is >= 2010 and <= 2020},
+        {"eyr", value => int.Parse(value) is >= 2020 and <= 2030},
+        {"hgt", value => {
+            var match = Regex.Match(value, @"(\d*)(.*)");
+            var height = int.Parse(match.Groups[1].Value);
+            var unit = match.Groups[2].Value;
+            if (unit == "cm") return height is >= 150 and <= 193;
+            if (unit == "in") return height is >= 59 and <= 76;
+            return false;
+        }},
+        {"hcl", value => Regex.IsMatch(value, @"#[0-9a-f]{6}")},
+        {"ecl", value => new List<string> {"amb","blu", "brn","gry","grn","hzl","oth"}.Contains(value)},
+        {"pid", value => Regex.IsMatch(value, @"^\d{9}$")},
+        {"cid", value => true}
+    };
+    return passport.All(field => validators[field.Key](field.Value));
 }
 
 IEnumerable<Dictionary<string, string>> LoadPassports()
@@ -44,17 +64,4 @@ IEnumerable<Dictionary<string, string>> LoadPassports()
     }
     passports.Add(passport);
     return passports;
-}
-
-
-public class Passport
-{
-    public int BirthYear { get; set; }
-    public int IssueYear { get; set; }
-    public int ExpirationYear { get; set; }
-    public string Height { get; set; }
-    public string HairColor { get; set; }
-    public string EyeColor { get; set; }
-    public int Id { get; set; }
-    public int CountryId { get; set; }
 }
