f33f6003f6872677c493195ba8e93e67f6d7640b
Adriaan <adriaan.putter@serko.com>
2020-12-08 16:52:13 +1300

    Day six part 1

---
+++ day-six/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllText("input.txt");
+
+var yesAnswers = input.Split("\n\n")
+                .Select(s => s.Replace("\n", "")
+                              .Distinct()
+                              .Count())
+                .Sum();
+
+Console.WriteLine($"Yes answers across groups: {yesAnswers}");
