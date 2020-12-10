ac7f5c0002b0d3e24dca90da637d0a4667fa2df9
Adriaan <adriaan.putter@serko.com>
2020-12-10 17:25:11 +1300

    Day seven part 1

---
+++ day-seven/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var bags = ParseInput(input);
+
+var result = FindShinyGoldHolderBags(bags, "shiny gold")
+.Distinct();
+
+Console.WriteLine(bags.Count);
+Console.WriteLine(result.Count());
+
+List<Bag> FindShinyGoldHolderBags(IEnumerable<Bag> bags, string bagName)
+{
+    var filtered = bags.Where(b => b.Bags.Exists(bagItem => bagItem.Name == bagName))
+               .ToList();
+    var result = new List<Bag>();
+    result.AddRange(filtered);
+    foreach(var filter in filtered) 
+    {
+        result.AddRange(FindShinyGoldHolderBags(bags, filter.Name));
+    }
+    return result;
+}
+
+List<Bag> ParseInput(string[] input)
+{
+    var list = new List<Bag>();
+    var containerBagRegex = new Regex(@"(\w+ \w+) bags contain (.+)");
+    var bagQtyRegex = new Regex(@"(\d) (\w+ \w+) bag");
+
+    foreach (var line in input)
+    {
+        var bagMatch = containerBagRegex.Match(line);
+        var bagItems = new List<BagItem>();
+
+        if (bagMatch.Groups[2].Value != "no other bags.")
+        {
+            foreach (var bagItem in bagMatch.Groups[2].Value.Split(','))
+            {
+                var bagItemMatch = bagQtyRegex.Match(bagItem);
+                bagItems.Add(new BagItem(bagItemMatch.Groups[2].Value, int.Parse(bagItemMatch.Groups[1].Value)));
+            }
+        }
+
+        list.Add(new Bag(bagMatch.Groups[1].Value, bagItems));
+    }
+
+    return list;
+}
+
+public record Bag(string Name, List<BagItem> Bags);
+public record BagItem(string Name, int Quantity);
