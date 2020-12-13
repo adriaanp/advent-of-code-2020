12900e093be5be93baf1b58e8efdb9155fb320ca
Adriaan <adriaan.putter@serko.com>
2020-12-14 09:50:32 +1300

    Day eight part 1

---
+++ day-eight/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var executeHistory = new List<int>();
+var accumulator = 0;
+var instructionPointer = 0;
+
+while (instructionPointer <= input.Length)
+{
+    if (executeHistory.Contains(instructionPointer))
+    {
+        Console.WriteLine($"Instruction repeated: {instructionPointer} with acc value {accumulator}.");
+        break;
+    }
+    executeHistory.Add(instructionPointer);
+    var regex = Regex.Match(input[instructionPointer], @"(.{3}) (.+)");
+    switch(regex.Groups[1].Value)
+    {
+        case "jmp":
+            instructionPointer += int.Parse(regex.Groups[2].Value);
+            break;
+        case "nop":
+            instructionPointer++;
+            break;
+        case "acc":
+            accumulator += int.Parse(regex.Groups[2].Value);
+            instructionPointer++;
+            break;
+    }
+}
