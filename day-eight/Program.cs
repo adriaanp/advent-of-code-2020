92609f759285d33e5b1f13e0edacfc1c5875a4b2
Adriaan <adriaan.putter@serko.com>
2020-12-14 17:22:23 +1300

    Day eight part 2

--- day-eight/Program.cs
+++ day-eight/Program.cs
@@ -1,34 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 var input = File.ReadAllLines("input.txt");
 
-var executeHistory = new List<int>();
-var accumulator = 0;
-var instructionPointer = 0;
+var jmp_nops = input
+                .Select((cmd, index) => new {Index = index, Cmd = cmd})
+                .Where(item => Regex.IsMatch(item.Cmd, "jmp|nop"))
+                .Select(item => item.Index)
+                .ToList();
 
-while (instructionPointer <= input.Length)
+foreach(var index in jmp_nops)
 {
-    if (executeHistory.Contains(instructionPointer))
+    var copyProgram = new string[input.Length];
+    input.CopyTo(copyProgram, 0);
+    var instruction = input[index];
+    if (instruction.StartsWith("jmp"))
     {
-        Console.WriteLine($"Instruction repeated: {instructionPointer} with acc value {accumulator}.");
+        copyProgram[index] = instruction.Replace("jmp", "nop");
+    }
+    else if (instruction.StartsWith("nop"))
+    {
+        copyProgram[index] = instruction.Replace("nop", "jmp");
+    }
+    var result = RunProgram(copyProgram);
+    if (result > -1)
+    {
+        Console.WriteLine($"Found bug at {index} - {instruction}");
+        Console.WriteLine($"Accumulator value is: {result}");
         break;
     }
-    executeHistory.Add(instructionPointer);
-    var regex = Regex.Match(input[instructionPointer], @"(.{3}) (.+)");
-    switch(regex.Groups[1].Value)
+}
+
+int RunProgram(string[] program)
+{
+    var executeHistory = new List<int>();
+    var accumulator = 0;
+    var instructionPointer = 0;
+
+    while (instructionPointer < program.Length)
     {
-        case "jmp":
-            instructionPointer += int.Parse(regex.Groups[2].Value);
-            break;
-        case "nop":
-            instructionPointer++;
-            break;
-        case "acc":
-            accumulator += int.Parse(regex.Groups[2].Value);
-            instructionPointer++;
-            break;
+        if (executeHistory.Contains(instructionPointer))
+        {
+            Console.WriteLine($"Instruction repeated: {instructionPointer} '{program[instructionPointer]}' with acc value {accumulator}.");
+            return -1;
+        }
+        executeHistory.Add(instructionPointer);
+        var regex = Regex.Match(program[instructionPointer], @"(.{3}) (.+)");
+        switch (regex.Groups[1].Value)
+        {
+            case "jmp":
+                instructionPointer += int.Parse(regex.Groups[2].Value);
+                break;
+            case "nop":
+                instructionPointer++;
+                break;
+            case "acc":
+                accumulator += int.Parse(regex.Groups[2].Value);
+                instructionPointer++;
+                break;
+        }
     }
+    return accumulator;
 }
