e37d36788ab8b48c81186439d4612271e9c31ba1
Adriaan Putter <adriaan.putter@gmail.com>
2020-12-15 00:12:26 +1300

    day nine part 2

--- day-nine/Program.cs
+++ day-nine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,16 +7,46 @@ var input = File.ReadAllLines("input.txt")
             .Select(line => long.Parse(line))
             .ToList();
 
-var pointer = 1;
-while (pointer < input.Count())
+var number = GetInvalidNumber(input);
+Console.WriteLine($"{number} is invalid");
+var weakness = GetXmasWeakness(number);
+Console.WriteLine($"encryption weakness {weakness}");
+
+long GetXmasWeakness(long number)
 {
-    var x = input.Skip(pointer - 1).Take(25).SelectMany(item => input.Skip(pointer - 1).Take(25), (x, y) => x == y ? 0 : x + y)
-            .ToList();
-    var number = input.Skip(pointer + 24).Take(1).Single();
-    if (!x.Contains(number))
+    var skip = 0;
+    for (int i = 0; i < input.Count(); i++)
     {
-        Console.WriteLine($"{number} is invalid in pointer {pointer}");
-        break;
+        var total = input.Skip(skip).Take(i).Sum();
+        if (total == number)
+        {
+            var min = input.Skip(skip).Take(i).Min();
+            var max = input.Skip(skip).Take(i).Max();
+            return min + max;
+        }
+        else if (total > number)
+        {
+            skip++;
+            i = 0;
+        }
     }
-    pointer++;
+    return -1;
 }
+
+long GetInvalidNumber(List<long> input)
+{
+    var pointer = 1;
+    while (pointer < input.Count())
+    {
+        var x = input.Skip(pointer - 1).Take(25).SelectMany(item => input.Skip(pointer - 1).Take(25), (x, y) => x == y ? 0 : x + y)
+                .ToList();
+        var number = input.Skip(pointer + 24).Take(1).Single();
+        if (!x.Contains(number))
+        {
+            return number;
+        }
+        pointer++;
+    }
+    return -1;
+}
+
