46ce9aedbbb6e451804e6e1d2bb72c2bbb07231a
Adriaan Putter <adriaan.putter@gmail.com>
2020-12-22 15:12:28 +1300

    Day eleven part 1

---
+++ day-eleven/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt");
+
+var seatMap = input.Select(line => line.ToList()).ToList();
+
+var currentSeatMap = seatMap;
+List<List<char>> nextSeatMap = new List<List<char>>();
+while (true)
+{
+    nextSeatMap = PeopleArriving(currentSeatMap);
+    if (CompareSeatMaps(nextSeatMap, currentSeatMap))
+    {
+        break;
+    }
+    currentSeatMap = nextSeatMap;
+}
+
+Console.WriteLine($"Total occupied seats: {nextSeatMap.SelectMany(x => x).Count(y => y == '#')}");
+
+List<List<char>> PeopleArriving(List<List<char>> seatMap)
+{
+    var newSeatMap = new List<List<char>>();
+    for (var x = 0; x < seatMap.Count; x++)
+    {
+        var newRow = new List<char>();
+        for (var y = 0; y < seatMap[x].Count; y++)
+        {
+            var seat = seatMap[x][y];
+            var seats = GetSeatsAround(seatMap, x, y);
+            var newSeat = seat;
+            if (seat == 'L' && seats.All(s => s != '#'))
+            {
+                // no occupied seats around then becomes occupied
+                newSeat = '#';
+            }
+            else if (seat == '#' && seats.Count(s => s == '#') >= 4)
+            {
+                // if four or more around then become unoccupied
+                newSeat = 'L';
+            }
+            newRow.Add(newSeat);
+        }
+        newSeatMap.Add(newRow);
+    }
+    return newSeatMap;
+}
+
+List<char> GetSeatsAround(List<List<char>> seatMap, int x, int y)
+{
+    return new List<char>
+    {
+        GetSeatAt(seatMap, x - 1, y - 1),
+        GetSeatAt(seatMap, x - 1, y),
+        GetSeatAt(seatMap, x - 1, y + 1),
+        GetSeatAt(seatMap, x, y - 1),
+        GetSeatAt(seatMap, x, y + 1),
+        GetSeatAt(seatMap, x + 1, y - 1),
+        GetSeatAt(seatMap, x + 1, y),
+        GetSeatAt(seatMap, x + 1, y + 1),
+    }
+    .Where(s => s != '*')
+    .ToList();
+}
+
+char GetSeatAt(List<List<char>> map, int x, int y){
+    if (x < 0 || y < 0 || x >= map.Count() || y >= map[0].Count())
+    {
+        return '*';
+    }
+    return map[x][y];
+};
+
+bool CompareSeatMaps(List<List<char>> a, List<List<char>> b) => Enumerable.Range(0, a.Count()).All(index => a[index].SequenceEqual(b[index]));
